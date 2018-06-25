0d8f1361115757ecd3efe349b86ab7367a131af3
blackstarbobby <bogdan.furniga@gmail.com>
2018-06-25 18:15:36 +0300

    education add functionality  started

--- resources/views/resume/edit.blade.php
+++ resources/views/resume/edit.blade.php
@@ -81,149 +81,7 @@
                                 @include('resume.edit.education')
                                 @include('resume.edit.experience')
 
-                                {{--</div>--}}
-                                {{--<div class="borderfull-width"></div>--}}
-                                {{--<div class="panel-heading">Education Details</div>--}}
-                                {{--<hr>--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>Basic / Graduation</label>--}}
-                                {{--<input type="text" class="form-control"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-r">--}}
-                                {{--<label>Specialization</label>--}}
-                                {{--<select class="form-control">--}}
-                                {{--<option>--- Choose a Category ---</option>--}}
-                                {{--<option>IT</option>--}}
-                                {{--<option>IT</option>--}}
-                                {{--<option>IT</option>--}}
-                                {{--</select>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>University / Institute</label>--}}
-                                {{--<input type="text" class="form-control"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-r">--}}
-                                {{--<label>Year</label>--}}
-                                {{--<select class="form-control">--}}
-                                {{--<option>--- Choose a Category ---</option>--}}
-                                {{--<option>IT</option>--}}
-                                {{--<option>IT</option>--}}
-                                {{--<option>IT</option>--}}
-                                {{--</select>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>Grading System</label>--}}
-                                {{--<select class="form-control">--}}
-                                {{--<option>--- Choose a Category ---</option>--}}
-                                {{--<option>IT</option>--}}
-                                {{--<option>IT</option>--}}
-                                {{--<option>IT</option>--}}
-                                {{--</select>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-r">--}}
-                                {{--<label>Marks</label>--}}
-                                {{--<input type="text" class="form-control"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="row">--}}
-                                {{--<div class="col-md-12">--}}
-                                {{--<button type="button" class="btn btn-click" data-toggle="collapse"--}}
-                                {{--data-target="#demo"><i class="fa fa-plus-circle"></i>Add Class XII--}}
-                                {{--</button>--}}
-                                {{--<div id="demo" class="collapse">--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>Basic / Institute</label>--}}
-                                {{--<input type="text" class="form-control"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>University / Institute</label>--}}
-                                {{--<input type="text" class="form-control"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>Marks</label>--}}
-                                {{--<input type="text" class="form-control"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>Year</label>--}}
-                                {{--<select class="form-control">--}}
-                                {{--<option>2017</option>--}}
-                                {{--<option>2016</option>--}}
-                                {{--<option>2015</option>--}}
-                                {{--<option>2014</option>--}}
-                                {{--</select>--}}
-                                {{--</div>--}}
-                                {{--</div>--}}
-                                {{--</div>--}}
-                                {{--</div>--}}
-                                {{--<div class="row">--}}
-                                {{--<div class="col-md-12">--}}
-                                {{--<button type="button" class="btn btn-click" data-toggle="collapse"--}}
-                                {{--data-target="#demo2"><i class="fa fa-plus-circle"></i>Add More--}}
-                                {{--Certificate--}}
-                                {{--</button>--}}
-                                {{--<div id="demo2" class="collapse">--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>University / Graduation</label>--}}
-                                {{--<input type="text" class="form-control"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>Marks</label>--}}
-                                {{--<input type="text" class="form-control"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>Year</label>--}}
-                                {{--<select class="form-control">--}}
-                                {{--<option>2017</option>--}}
-                                {{--<option>2016</option>--}}
-                                {{--<option>2015</option>--}}
-                                {{--<option>2014</option>--}}
-                                {{--</select>--}}
-                                {{--</div>--}}
-                                {{--</div>--}}
-                                {{--</div>--}}
-                                {{--</div>--}}
-                                {{--<div class="borderfull-width"></div>--}}
-                                {{--<div class="panel-heading">Employer/Designation</div>--}}
-                                {{--<hr>--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>Employer Name</label>--}}
-                                {{--<input type="text" class="form-control"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>Status</label>--}}
-                                {{--<select class="form-control">--}}
-                                {{--<option>Previous Employer</option>--}}
-                                {{--<option>Employer</option>--}}
-                                {{--<option>Employer</option>--}}
-                                {{--<option>Employer</option>--}}
-                                {{--</select>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-3 p-l">--}}
-                                {{--<label>Duration</label>--}}
-                                {{--<input type="date" class="form-control" placeholder="15 January 2014"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-3 p-l">--}}
-                                {{--<label></label>--}}
-                                {{--<input type="date" class="form-control" placeholder="15 January 2014"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-6 p-l">--}}
-                                {{--<label>Designation</label>--}}
-                                {{--<input type="text" class="form-control"/>--}}
-                                {{--</div>--}}
-                                {{--<div class="form-group col-md-12 p-l">--}}
-                                {{--<label>Job Profile</label>--}}
-                                {{--<textarea type="text" class="form-control" placeholder=""></textarea>--}}
-                                {{--</div>--}}
-                                {{--<div class="row">--}}
-                                {{--<div class="col-md-12">--}}
-                                {{--<button type="button" class="btn btn-click" data-toggle="collapse"--}}
-                                {{--data-target="#demo3"><i class="fa fa-plus-circle"></i>Add More--}}
-                                {{--</button>--}}
-                                {{--<div id="demo3" class="collapse"></div>--}}
-                                {{--</div>--}}
-                                {{--</div>--}}
-                                {{--<div class="col-md-4 p-l">--}}
-                                {{--<a href="#" class="btn btn-default btn-block">Preview Your Resume</a>--}}
-                                {{--</div>--}}
+
                                 <div class="col-md-4 p-l">
                                     <button type="submit" class="btn btn-default btn-block saveCv">Salveaza
                                         modificarile
@@ -279,6 +137,20 @@
 
         $("#imageUpload").change(function () {
             readURL(this);
+        });
+
+
+        $('.add-school').on('click', function () {
+            console.log('here');
+            let $template = $('.school-template');
+
+            if ($template.length) {
+                let $copyTemplate = $template.clone();
+                console.log('here2');
+                $copyTemplate.removeClass('school-template');
+                $('.education-container').append($copyTemplate);
+            }
+
         });
     </script>
 @endsection
--- resources/views/resume/edit/education.blade.php
+++ resources/views/resume/edit/education.blade.php
@@ -4,87 +4,116 @@
 <hr>
 
 @foreach($resume->education as $school)
+    <div class="education-container">
+        <div class="school">
+            <div class="row">
+                <div class="form-group col-md-6">
+                    <label>Numele Institutiei</label>
+                    <input type="text" class="form-control" name="value[education][school][institution][]"
+                           value=""
+                    />
+                </div>
 
-    <div class="school">
-        <div class="row">
-            <div class="form-group col-md-6">
-                <label>Numele Institutiei</label>
-                <input type="text" class="form-control" name="value[education][school][institution]"
-                       value="{{$resume->basic->first_name ?? null}}"
-                />
-                @if ($errors->has('value.basic.first_name'))
-                    <span class="invalid-feedback">
-                        <strong>{{ $errors->first('value.basic.first_name') }}</strong>
-                    </span>
-                @endif
+                <div class="form-group col-md-6">
+                    <label>Profil / Specializare</label>
+                    <input type="text" class="form-control" name="value[education][school][institution][]"
+                           value=""
+                    />
+                </div>
             </div>
 
-            <div class="form-group col-md-6">
-                <label>Profil / Specializare</label>
-                <input type="text" class="form-control" name="value[education][school][institution]"
-                       value="{{$resume->basic->first_name ?? null}}"
-                />
-                @if ($errors->has('value.basic.first_name'))
-                    <span class="invalid-feedback">
-            <strong>{{ $errors->first('value.basic.first_name') }}</strong>
-                    </span>
-                @endif
-            </div>
-        </div>
+            <div class="row">
+                <div class="form-group col-md-6">
+                    <label>Tipul Institutiei</label>
+                    <input type="text" class="form-control" name="value[education][school][type][]"
+                           placeholder="Facultate \ Masterat \ Doctorat"
+                           value=""
+                    />
+                </div>
 
-        <div class="row">
-            <div class="form-group col-md-6">
-                <label>Tipul Institutiei</label>
-                <input type="text" class="form-control" name="value[education][school][institution]"
-                       placeholder="Facultate \ Masterat \ Doctorat"
-                       value="{{$resume->basic->first_name ?? null}}"
-                />
-                @if ($errors->has('value.basic.first_name'))
-                    <span class="invalid-feedback">
-                        <strong>{{ $errors->first('value.basic.first_name') }}</strong>
-                    </span>
-                @endif
+                <div class="form-group col-md-6">
+                    <label>Oras</label>
+                    <input type="text" class="form-control" name="value[education][school][city][]"
+                           value=""
+                    />
+                </div>
             </div>
 
-            <div class="form-group col-md-6">
-                <label>Oras</label>
-                <input type="text" class="form-control" name="value[education][school][institution]"
-                       value="{{$resume->basic->first_name ?? null}}"
-                />
-                @if ($errors->has('value.basic.first_name'))
-                    <span class="invalid-feedback">
-            <strong>{{ $errors->first('value.basic.first_name') }}</strong>
-                    </span>
-                @endif
+            <div class="row">
+                <div class="form-group col-md-6">
+                    <label>Data Admiterii</label>
+                    <input type="date" class="form-control" name="value[education][school][start][]"
+                           value="{{$resume->basic->birth_date ?? null}}"
+                    />
+                </div>
+
+                <div class="form-group col-md-6">
+                    <label>Data Absolvirii</label>
+                    <input type="date" class="form-control" name="value[education][school][end][]"
+                           value="{{$resume->basic->birth_date ?? null}}"
+                    />
+                </div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="form-group col-md-6">
-                <label>Data Admiterii</label>
-                <input type="date" class="form-control" name="value[basic][birth_date]"
-                       value="{{$resume->basic->birth_date ?? null}}"
-                />
-                @if ($errors->has('value.basic.birth_date'))
-                    <span class="invalid-feedback">
-                        <strong>{{ $errors->first('value.basic.birth_date') }}</strong>
-                    </span>
-                @endif
-            </div>
-
-            <div class="form-group col-md-6">
-                <label>Data Absolvirii</label>
-                <input type="date" class="form-control" name="value[basic][birth_date]"
-                       value="{{$resume->basic->birth_date ?? null}}"
-                />
-                @if ($errors->has('value.basic.birth_date'))
-                    <span class="invalid-feedback">
-                        <strong>{{ $errors->first('value.basic.birth_date') }}</strong>
-                    </span>
-                @endif
-            </div>
-        </div>
-
     </div>
 
 @endforeach
+
+<div class="row">
+    <div class="col-md-12">
+        <a class="btn btn-default pull-right add-school">Adauga</a>
+    </div>
+</div>
+
+
+<div class="school-template">
+    <div class="row">
+        <div class="form-group col-md-6">
+            <label>Numele Institutiei</label>
+            <input type="text" class="form-control" name="value[education][school][institution][]"
+                   value=""
+            />
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>Profil / Specializare</label>
+            <input type="text" class="form-control" name="value[education][school][institution][]"
+                   value=""
+            />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="form-group col-md-6">
+            <label>Tipul Institutiei</label>
+            <input type="text" class="form-control" name="value[education][school][type][]"
+                   placeholder="Facultate \ Masterat \ Doctorat"
+                   value=""
+            />
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>Oras</label>
+            <input type="text" class="form-control" name="value[education][school][city][]"
+                   value=""
+            />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="form-group col-md-6">
+            <label>Data Admiterii</label>
+            <input type="date" class="form-control" name="value[education][school][start][]"
+                   value=""
+            />
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>Data Absolvirii</label>
+            <input type="date" class="form-control" name="value[education][school][end][]"
+                   value=""
+            />
+        </div>
+    </div>
+
+</div>
